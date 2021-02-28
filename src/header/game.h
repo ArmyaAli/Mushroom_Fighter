6e792ba33ef65ac95fde4df478fd16e093d2b71f
ArmyaAli <ali.umar.work@gmail.com>
2021-02-28 12:48:59 -0500

    globals..

---
+++ src/header/game.h
@@ -0,0 +1,48 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "menu.h"
+#include "player.h"
+
+typedef enum gameState
+{
+    PLAY,
+    PAUSE,
+    MENU,
+    HELP
+} STATE;
+
+typedef struct frameInformation
+{
+    int currentFrame;
+    int framesCounter;
+    int framesSpeed;
+    Rectangle frameRec;
+} frameInformation;
+
+// LIFECYCLE
+void Init();
+void Update();
+void Draw();
+void Destroy();
+
+void initGame();
+void initFrameData(Texture2D* sheet);
+void controlAnimation(frameInformation* data, Texture2D* sheet);
+void cropSpriteSheetOnDirection(Texture2D* sheet);
+void updateGameState();
+
+extern int screenHeight;
+extern int screenWidth;
+extern Vector2 playerPosition;
+extern STATE currentState;
+extern frameInformation frameData;
+extern moveDirection dir;
+extern menuButtons STRUCT_MENU_BUTTONS;
+
+extern Image playerImage;
+extern Image playerSpriteSheet;
+extern Texture2D Player;
+extern Texture2D sheet;
+
+#endif
