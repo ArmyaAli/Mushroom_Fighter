81fc6fa7fb8fd73a31d221b11c6e3354caa854bd
Ali Umar <ali.umar.work@gmail.com>
2023-07-18 10:27:39 -0400

    ft: add game_state handler

--- src/main.c
+++ src/main.c
@@ -1,25 +1,46 @@
 #include "raylib.h"
-#include "constants.h"
 #include "logger.h"
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 400
+#include "render.h"
+#include "constants.h"
 
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
 
-int main(void)
+extern int CURR_GAME_STATE;
+
+// Entry point 
+int main()
 {
+    // SetConfigFlags(FLAG_WINDOW_UNDECORATED);
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Mushroom Fighter");
 
-    SetTargetFPS(60);               
+    SetTargetFPS(60);
 
-    LOG(DEBUG, "Hello my name is Ali");
-    while (!WindowShouldClose())    
+    while (!WindowShouldClose()) // Detect window close button or ESC key
     {
         BeginDrawing();
+
         ClearBackground(RAYWHITE);
+
+        resolveGameState();
+
         EndDrawing();
     }
 
-    CloseWindow();
+    CloseWindow(); 
 
     return 0;
+}
+
+resolveGameState() {
+    switch(CURR_GAME_STATE) {
+        case MENU:
+            draw_menu(); break;
+        case GAME:
+            draw_game(); 
+            update_game();
+            break;
+        case GAME_PAUSED:
+            break;
+    }
 }
