6dec042942fde57148a101d6c617ea0efeaa2858
Ali Umar <ali.umar.work@gmail.com>
2023-07-18 10:29:05 -0400

    file: renderer header

---
+++ include/render.h
@@ -0,0 +1,15 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include "raylib.h"
+
+void init_game();
+void init_grid();
+
+void draw_game();
+// void draw_grid();
+void draw_menu();
+
+void update_game();
+
+#endif
